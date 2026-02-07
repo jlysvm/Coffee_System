57392580b8bdfd41eaa869128fa552fd905bed62
unknown <christianagot11@gmail.com>
2026-02-08 02:11:03 +0800

    changing to supabase as backend db and storage provider

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,8 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    implementation ("io.appwrite:sdk-for-android:12.0.0")
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.0.0"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:storage-kt")
+    implementation("io.ktor:ktor-client-android:3.0.0")
 }
