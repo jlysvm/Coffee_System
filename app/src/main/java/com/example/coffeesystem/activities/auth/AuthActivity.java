0e86a6487e9cc53bf1ce0b2a0c3cfd39cf58632d
unknown <christianagot11@gmail.com>
2026-02-08 17:31:52 +0800

    added authenticated user variable

--- app/src/main/java/com/example/coffeesystem/activities/auth/AuthActivity.java
+++ app/src/main/java/com/example/coffeesystem/activities/auth/AuthActivity.java
@@ -5,13 +5,23 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.coffeesystem.R;
+import com.example.coffeesystem.models.User;
 
 public class AuthActivity extends AppCompatActivity {
+
+    private static User authenticatedUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
     }
-}
 
+    public static User getAuthenticatedUser() {
+        return authenticatedUser;
+    }
+
+    public static void setAuthenticatedUser(User authenticatedUser) {
+        AuthActivity.authenticatedUser = authenticatedUser;
+    }
+}
