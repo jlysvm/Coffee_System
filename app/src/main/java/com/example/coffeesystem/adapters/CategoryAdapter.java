be59ad2e729bcd5e84ce69267dcd37049e3e6748
unknown <christianagot11@gmail.com>
2026-02-08 21:57:17 +0800

    added CategoryRepository class, CategoryAdapter class, and text_category.xml

---
+++ app/src/main/java/com/example/coffeesystem/adapters/CategoryAdapter.java
@@ -0,0 +1,52 @@
+package com.example.coffeesystem.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.coffeesystem.R;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+    private Context mContext;
+    private List<String> categories;
+
+    public CategoryAdapter(Context context, List<String> categories) {
+        this.mContext = context;
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public CategoryAdapter.CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.text_category, parent, false);
+        return new CategoryAdapter.CategoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.CategoryViewHolder holder, int position) {
+        String category = categories.get(position);
+        holder.categoryName.setText(category);
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public static class CategoryViewHolder extends RecyclerView.ViewHolder {
+        TextView categoryName;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            categoryName = itemView.findViewById(R.id.drink_category);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/coffeesystem/repository/CategoryRepository.java
@@ -0,0 +1,63 @@
+package com.example.coffeesystem.repository;
+
+import android.util.Log;
+
+import com.example.coffeesystem.BuildConfig;
+import com.example.coffeesystem.callbacks.FetchCallback;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class CategoryRepository {
+    private static final String supabaseUrl = BuildConfig.SUPABASE_URL;
+    private static final String supabaseKey = BuildConfig.SUPABASE_KEY;
+
+    public void getCategories(FetchCallback<List<String>> callback) {
+        OkHttpClient client = new OkHttpClient();
+
+        String url = supabaseUrl+"/rest/v1/categories?select=name";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("apikey", supabaseKey)
+                .addHeader("Authorization", "Bearer " + supabaseKey)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        new Thread(() -> {
+            try (Response response = client.newCall(request).execute()) {
+                String responseBody = response.body().string();
+                Log.i("Supabase", "Response body: " + responseBody);
+
+                if (!response.isSuccessful()) {
+                    callback.onError(response.code());
+                    return;
+                }
+                if (responseBody.equals("[]")) {
+                    callback.onNotFound();
+                    return;
+                }
+
+                JSONArray jsonArray = new JSONArray(responseBody);
+                List<String> categories = new java.util.ArrayList<>();
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject obj = jsonArray.getJSONObject(i);
+                    categories.add(obj.getString("name"));
+                }
+
+                callback.onSuccess(categories);
+
+            } catch (Exception e) {
+                Log.e("Login", "Error: ", e);
+                callback.onNetworkError(e);
+            }
+        }).start();
+    }
+}
